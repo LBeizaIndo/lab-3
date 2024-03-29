7f659c505a1304bc0d3509a9e2177aa310b73128
LBeizaIndo <lfbeiza@miuandes.com>
2024-03-29 15:51:40 -0300

    correction

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get '/', to: 'static_pages#home'
+  get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 end
